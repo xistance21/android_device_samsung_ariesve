d0cbf26371cab2f3a4ceeed2af7d3ca2af0f916a
Emir Unluturk <cenonx@gmail.com>
2013-02-23 16:50:39 +0200

    Some more derp reduction

--- ota-update-center.mk
+++
@@ -1,9 +0,0 @@
-ADDITIONAL_BUILD_PROPERTIES += \
-      otaupdater.otaid=ivendorcm10 \
-      otaupdater.otaver=$(CM_NUMERICAL_VERSION) \
-      otaupdater.otatime=$(shell date +%Y%m%d-%H%M)	
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/OTAUpdater.apk:system/app/OTAUpdater.apk
-
-
